61b6698e6caa8003c029ee29c3643e8eb1db1744
kenzho07 <you@example.com>
2024-04-20 01:41:25 +0800

    Adding Social Media Skills

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ericsonciriaco-reactportfolio/precache-manifest.c5ae8882a7d7ac4b6447688008a3d6f1.js"
+  "/ericsonciriaco-reactportfolio/precache-manifest.fdd4ff1796c6eb1df0136081e78e85ba.js"
 );
 
 self.addEventListener('message', (event) => {
