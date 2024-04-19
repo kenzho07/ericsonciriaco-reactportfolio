91f747073f33c8e9c3b7d87d21827d5cda24cc0e
kenzho07 <you@example.com>
2024-04-20 00:19:25 +0800

    add My skills status

---
+++ build/precache-manifest.c5ae8882a7d7ac4b6447688008a3d6f1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3441b35cc02e95275d890c954f5c91b1",
+    "url": "/ericsonciriaco-reactportfolio/index.html"
+  },
+  {
+    "revision": "e1969bf7692d56cda44b",
+    "url": "/ericsonciriaco-reactportfolio/static/css/2.e15a0812.chunk.css"
+  },
+  {
+    "revision": "9ad0a6b90ce2a00438da",
+    "url": "/ericsonciriaco-reactportfolio/static/css/main.725012c2.chunk.css"
+  },
+  {
+    "revision": "e1969bf7692d56cda44b",
+    "url": "/ericsonciriaco-reactportfolio/static/js/2.ce0e9aa1.chunk.js"
+  },
+  {
+    "revision": "abce24152a0176f228dfcad2f6d860ba",
+    "url": "/ericsonciriaco-reactportfolio/static/js/2.ce0e9aa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad0a6b90ce2a00438da",
+    "url": "/ericsonciriaco-reactportfolio/static/js/main.adb8305f.chunk.js"
+  },
+  {
+    "revision": "7cff6d1a23bd7adbc842",
+    "url": "/ericsonciriaco-reactportfolio/static/js/runtime-main.a34c4699.js"
+  },
+  {
+    "revision": "6763d2d35906b505b2c1c2557d1a8be1",
+    "url": "/ericsonciriaco-reactportfolio/static/media/formal.6763d2d3.jpg"
+  },
+  {
+    "revision": "464e5b632312f9e99a696d1d4cf8225e",
+    "url": "/ericsonciriaco-reactportfolio/static/media/kleengreen.464e5b63.jpg"
+  },
+  {
+    "revision": "4f01dbe03c2d2ca6eb172feefbd6029f",
+    "url": "/ericsonciriaco-reactportfolio/static/media/mirex.4f01dbe0.png"
+  },
+  {
+    "revision": "6763d2d35906b505b2c1c2557d1a8be1",
+    "url": "/ericsonciriaco-reactportfolio/static/media/self.6763d2d3.jpg"
+  },
+  {
+    "revision": "5fc5e8a42c14fdb83899a75dbf76c023",
+    "url": "/ericsonciriaco-reactportfolio/static/media/tao.5fc5e8a4.png"
+  }
+]);
